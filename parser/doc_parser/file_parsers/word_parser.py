726062b3fc41779c989ef6981d1d82d83409f038
TheRealMizAL <50375436+TheRealMizAL@users.noreply.github.com>
2022-01-28 12:30:33 +0500

    end docx parser

--- parser/doc_parser/file_parsers/word_parser.py
+++ parser/doc_parser/file_parsers/word_parser.py
@@ -2,23 +2,34 @@ from .abc import AbstractDocument
 from io import BytesIO
 import docx
 import docx2txt
+import os
 
 
 class WordDocument(AbstractDocument):
 
     def __init__(self):
+        self.__IMAGES_PATH = './images'
         self.__changes = {'group_name': 'ТО-21-1',
-                          'changelist': []}
+                          'changelist': [],
+                          'images': []}
+
+        try:
+            os.mkdir(self.__IMAGES_PATH)
+        except FileExistsError:
+            pass
 
     async def parse(self, file_bytes):
 
         docx_file = docx.Document(BytesIO(file_bytes))
-
         await self.__tables_parse(docx_file.tables)
 
-        # await self.__images_parse(docx_file.images)
-        # todo: add image parser
-        return 'word parser'
+        docx2txt.process(BytesIO(file_bytes), self.__IMAGES_PATH)
+        for image in os.listdir(self.__IMAGES_PATH):
+            with open(f'{self.__IMAGES_PATH}/{image}', 'r') as img:
+                self.__changes['images'].append(img)
+
+        print(self.__changes)
+        return self.__changes
 
     async def __tables_parse(self, tables):
         for table in tables:
@@ -33,7 +44,3 @@ class WordDocument(AbstractDocument):
                         self.__changes['changelist'].append({'number': int(lesson_number) if lesson_number != '' else None,
                                                              'lesson_name': lesson_name,
                                                              'teacher_name': teacher_name})
-
-    async def __images_parse(self, images):
-        for image in images:
-            print(image)
